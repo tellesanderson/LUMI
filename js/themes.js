e0e153700ad41687f76686afc9a375ef656fffe8
Anderson Telles <tellesanderson@users.noreply.github.com>
2026-08-12 21:06:45 -0300

    perf: converter todas as imagens existentes do site para o formato WebP otimizado

--- js/themes.js
+++ js/themes.js
@@ -7,105 +7,105 @@ const defaultThemes = [
     id: "trolls",
     title: "Trolls",
     category: "unissex",
-    coverImage: "img/themes/trolls.png",
+    coverImage: "img/themes/trolls.webp",
     galleryImages: []
   },
   {
     id: "hulk",
     title: "Hulk",
     category: "meninos",
-    coverImage: "img/themes/hulk.png",
+    coverImage: "img/themes/hulk.webp",
     galleryImages: []
   },
   {
     id: "homem_aranha",
     title: "Homem Aranha",
     category: "meninos",
-    coverImage: "img/themes/homem_aranha.png",
+    coverImage: "img/themes/homem_aranha.webp",
     galleryImages: []
   },
   {
     id: "lol",
     title: "LOL Surprise",
     category: "meninas",
-    coverImage: "img/themes/lol.png",
+    coverImage: "img/themes/lol.webp",
     galleryImages: []
   },
   {
     id: "fortnite",
     title: "Fortnite",
     category: "meninos",
-    coverImage: "img/themes/fortnite.png",
+    coverImage: "img/themes/fortnite.webp",
     galleryImages: []
   },
   {
     id: "ninjago",
     title: "Ninjago",
     category: "meninos",
-    coverImage: "img/themes/ninjago.png",
+    coverImage: "img/themes/ninjago.webp",
     galleryImages: []
   },
   {
     id: "homem_ferro",
     title: "Homem de Ferro",
     category: "meninos",
-    coverImage: "img/themes/homem_ferro.png",
+    coverImage: "img/themes/homem_ferro.webp",
     galleryImages: []
   },
   {
     id: "roblox",
     title: "Roblox",
     category: "unissex",
-    coverImage: "img/themes/roblox.png",
+    coverImage: "img/themes/roblox.webp",
     galleryImages: []
   },
   {
     id: "flash",
     title: "Flash",
     category: "meninos",
-    coverImage: "img/themes/flash.png",
+    coverImage: "img/themes/flash.webp",
     galleryImages: []
   },
   {
     id: "freefire",
     title: "Free Fire",
     category: "meninos",
-    coverImage: "img/themes/freefire.png",
+    coverImage: "img/themes/freefire.webp",
     galleryImages: []
   },
   {
     id: "palmeiras",
     title: "Palmeiras",
     category: "meninos",
-    coverImage: "img/themes/palmeiras.png",
+    coverImage: "img/themes/palmeiras.webp",
     galleryImages: []
   },
   {
     id: "stumbleguys",
     title: "Stumble Guys",
     category: "unissex",
-    coverImage: "img/themes/stumbleguys.png",
+    coverImage: "img/themes/stumbleguys.webp",
     galleryImages: []
   },
   {
     id: "magali",
     title: "Magali",
     category: "meninas",
-    coverImage: "img/themes/magali.png",
+    coverImage: "img/themes/magali.webp",
     galleryImages: []
   },
   {
     id: "capivara",
     title: "Capivara",
     category: "unissex",
-    coverImage: "img/themes/capivara.png",
+    coverImage: "img/themes/capivara.webp",
     galleryImages: []
   },
   {
     id: "stitch",
     title: "Stitch",
     category: "unissex",
-    coverImage: "img/themes/stitch.png",
+    coverImage: "img/themes/stitch.webp",
     galleryImages: []
   }
 ];
